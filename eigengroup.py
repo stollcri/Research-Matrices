e6e6076c6104decdbd22cb5551ebb714077c9565
Christopher Stoll <stollcri@gmail.com>
2014-04-06 21:25:21 -0400

    Now gives some useable results
    Not very good, but useable for now

--- eigengroup.py
+++ eigengroup.py
@@ -36,12 +36,14 @@ def read_images(source_directory):
 	filenames = []
 	eigen_images = []
 	dirname = source_directory[0]
+	print dirname
 	if os.path.isdir(dirname):
 		for filename in os.listdir(dirname):
-			if os.path.exists(os.path.join(dirname, filename)):
-				image_as_vector = add_to_matrix_from_file(os.path.join(dirname, filename))
-				eigen_images.append(image_as_vector[:])
-				filenames.append(filename)
+			if filename[0] != '.':
+				if os.path.exists(os.path.join(dirname, filename)):
+					image_as_vector = add_to_matrix_from_file(os.path.join(dirname, filename))
+					eigen_images.append(image_as_vector[:])
+					filenames.append(filename)
 	else:
 		for filename in source_directory:
 			if os.path.exists(filename):
@@ -90,28 +92,50 @@ def add_to_bin(bins, item_a, item_b):
 	bin_found = False
 
 	for x in xrange(0, bin_count):
+		if bins[x][0] == item_a or bins[x][0] == item_b:
+			if bins[x][0] == item_a:
+				unbined = item_b
+			else:
+				unbined = item_a
+
+			for y in xrange(0, bin_count):
+				if bins[x][y] == unbined:
+					break
+				if bins[x][y] == -1:
+					bins[x][y] = unbined
+					break
+			bin_found = True
+
 		if bins[x][0] == -1:
-			if not bin_found:
-				bins[x][0] = item_a
-				bins[x][1] = item_b
-				bin_found = True
+			bin_found = True
+			bins[x][0] = item_a
+			bins[x][1] = item_b
+
+		if bin_found:
 			break
 
-		for y in xrange(0, bin_count):
-			if bins[x][y] == -1:
-				if bin_found:
-					bins[x][y] = unbined
-				break
+		# if bins[x][0] == -1:
+		# 	if not bin_found:
+		# 		bins[x][0] = item_a
+		# 		bins[x][1] = item_b
+		# 		bin_found = True
+		# 	break
 
-			if bins[x][y] == item_a or bins[x][y] == item_b:
-				if bin_found:
-					break
+		# for y in xrange(0, bin_count):
+		# 	if bins[x][y] == -1:
+		# 		if bin_found:
+		# 			bins[x][y] = unbined
+		# 		break
 
-				bin_found = True
-				if bins[x][y] == item_a:
-					unbined = item_b
-				elif bins[x][y] == item_b:
-					unbined = item_a
+		# 	if bins[x][y] == item_a or bins[x][y] == item_b:
+		# 		if bin_found:
+		# 			break
+
+		# 		bin_found = True
+		# 		if bins[x][y] == item_a:
+		# 			unbined = item_b
+		# 		elif bins[x][y] == item_b:
+		# 			unbined = item_a
 	return bins
 
 
@@ -124,8 +148,10 @@ def show_eigen_groups(eigen_images, eigen_files):
 			scores[col_index][row_index] = cosine_similarity(col_image, row_image)
 
 	bins = [[-1 for j in xrange(width)] for i in xrange(width)]
+	possible_items = []
 
 	for x in xrange(0, width):
+		possible_items.append(str(eigen_files[x].split('_')[0]))
 		print eigen_files[x].split('_')[0], ':',
 		for y in xrange(x, width):
 			if scores[x][y] > 99:
@@ -137,19 +163,49 @@ def show_eigen_groups(eigen_images, eigen_files):
 		# print eigen_files[x].split('_')[0]
 
 	bin_count = len(bins)
+	used_list = []
+	lone_count = 0
 	print
 	for x in xrange(0, bin_count):
 		if bins[x][0] == -1:
 			break
 
+		all_used = False
+		y_used = 0
+		z_used = 0
 		for y in xrange(0, bin_count):
 			if bins[x][y] == -1:
 				break
-			else:
-				print bins[x][y],
-		print
-		print
+			y_used += 1
+			for z in xrange(0, len(used_list)):
+				if bins[x][y] == used_list[z]:
+					z_used += 1
+					break
+		if z_used >= y_used:
+			all_used = True
 
+		if not all_used:
+			output = ""
+			for y in xrange(0, bin_count):
+				if bins[x][y] == -1:
+					break
+				else:
+					used_list.append(bins[x][y])
+					output = output + bins[x][y]
+			lone_count += 1
+			print output
+
+	for x in xrange(0, len(possible_items)):
+		for y in xrange(0, len(used_list)):
+			if used_list[y] == possible_items[x]:
+				possible_items[x] = -1
+
+	for x in xrange(0, len(possible_items)):
+		if possible_items[x] != -1:
+			lone_count += 1
+			print possible_items[x]
+
+	print lone_count
 
 def create_eigenimage(source_directory):
 	eigen_files, eigen_images = read_images(source_directory)
